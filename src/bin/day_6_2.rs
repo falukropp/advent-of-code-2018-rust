136885c984f143fb38eb56213eed42b89e2d6bf6
Generalelliot <generalelliot@gmail.com>
2018-12-06 23:28:34 +0100

    Ok, 6_2 wasn't that complicated to BRUTE

--- src/bin/day_6_2.rs
+++ src/bin/day_6_2.rs
@@ -6,31 +6,42 @@ use std::io::BufReader;
 
 type GenError = Box<std::error::Error>;
 
-fn process_list(coords: &Vec<Vec<u32>>) -> Result<i32, GenError> {
-    let number_of_coords = coords.len();
-    let mid_point_row: f64 =
-        coords.iter().map(|v| v.get(1).unwrap()).sum().unwrap() / number_of_coords;
-    let mid_point_col: f64 =
-        coords.iter().map(|v| v.get(0).unwrap()).sum().unwrap() / number_of_coords;
+fn process_list(coords: &Vec<Vec<i32>>, max_distance: i32) -> Result<i32, GenError> {
+    let max_row = *coords.iter().map(|v| v.get(1).unwrap()).max().unwrap() as usize;
+    let max_col = *coords.iter().map(|v| v.get(0).unwrap()).max().unwrap() as usize;
 
-    return Ok(0);
+    // BRUTE FORCE !!!!!!
+    let mut region_size = 0;
+    for y in 0..max_row + 1 {
+        for x in 0..max_col + 1 {
+            let distance: i32 = coords
+                .iter()
+                .map(|c| (c[0] - x as i32).abs() + (c[1] - y as i32).abs())
+                .sum();
+            if distance < max_distance {
+                region_size += 1;
+            }
+        }
+    }
+
+    return Ok(region_size);
 }
 
-fn process_file(path: &str) -> Result<i32, GenError> {
+fn process_file(path: &str, max_distance: i32) -> Result<i32, GenError> {
     let f = File::open(path)?;
     let r = BufReader::new(f);
 
     let mut coords = Vec::new();
 
     r.lines().map(|l| l.unwrap()).for_each(|line| {
-        let coord: Vec<u32> = line.split(",").map(|i| i.trim().parse().unwrap()).collect();
+        let coord: Vec<i32> = line.split(",").map(|i| i.trim().parse().unwrap()).collect();
         coords.push(coord);
     });
 
-    return process_list(&coords);
+    return process_list(&coords, max_distance);
 }
 
 fn main() {
-    let sum = process_file("src/res/day_6.txt").unwrap();
+    let sum = process_file("src/res/day_6.txt", 10000).unwrap();
     println!("Checksum {}", sum);
 }
